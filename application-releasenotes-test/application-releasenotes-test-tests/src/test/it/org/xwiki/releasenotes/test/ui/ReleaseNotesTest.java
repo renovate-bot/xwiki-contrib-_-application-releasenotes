a7f9dbb85c16fe3b51c446b1910cae3228aacb99
Vincent Massol <vincent@massol.net>
2016-09-27 19:02:42 +0200

    RN-1: First version of the Release Notes application

---
+++ application-releasenotes-test/application-releasenotes-test-tests/src/test/it/org/xwiki/releasenotes/test/ui/ReleaseNotesTest.java
@@ -0,0 +1,38 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.releasenotes.test.ui;
+
+import org.junit.Test;
+import org.xwiki.test.ui.AbstractTest;
+
+import org.junit.Assert;
+
+/**
+ * UI tests for the Release Notes Application.
+ *
+ * @version $Id$
+ */
+public class ReleaseNotesTest extends AbstractTest
+{
+    @Test
+    public void verifyFeatures()
+    {
+    }
+}
